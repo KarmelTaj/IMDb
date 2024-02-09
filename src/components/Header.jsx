c87bd0684d38c5c54b4be6ab4b95608c575426d6
Karmel <karmeltajeddini82@gmail.com>
2024-02-09 07:14:52 +0330

    Changed Header to be more like IMDb

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,94 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import LiveTvIcon from '@mui/icons-material/LiveTv';
+import { createTheme, ThemeProvider, styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+
+const theme = createTheme({
+    spacing: 8,
+    palette: {
+        primary: {
+            main: '#121212',
+            contrastText: '#fff',
+        },
+        secondary: {
+            main: '#f5c518',
+            contrastText: '#000',
+        },
+    },
+});
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 1),
+    '&:focus-within': {
+        borderColor: '#f5c518',
+    },
+    borderWidth: '2px',
+    borderStyle: 'solid',
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: '#212121',
+    width: '100%',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(0.4, 1, 0.4, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(5)})`,
+        transition: theme.transitions.create('width'),
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '25ch',
+            },
+        },
+    },
+}));
 
 const Header = () => {
+
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ py: 1.5 }}>
-                <Toolbar variant='dense' sx={{p: 0}}>
-                    <LiveTvIcon fontSize='large' edge="start" color="inherit" sx={{ mr: 3 }}/>
-                    <Typography variant="h6" component="div">
-                        Movies
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" sx={{ py: 1.5 }}>
+                    <Toolbar variant='dense' sx={{ p: 0 }}>
+                        <Typography variant="h6" component="div" sx={{ fontWeight: 800, bgcolor: 'secondary.main', color: 'secondary.contrastText', borderRadius: '4px', px: 0.5 }}>
+                            IMDb
+                        </Typography>
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon sx={{color: '#868686'}}/>
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </ThemeProvider>
     );
 }
 
