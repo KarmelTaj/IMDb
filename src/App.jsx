251088ad8d2cb731efedd74fbf6cf8a7d6277704
Karmel <karmeltajeddini82@gmail.com>
2024-02-12 06:29:10 +0330

    Add sign in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/Header'
 import Home from './components/Home/Home'
+import SignIn from './components/SignIn/SignIn'
 
 
 function App() {
-  return <Home />
+  return <>
+  <SignIn />
+  </>
 }
 
 export default App
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Header";
 import MainHome from "./MainHome";
-import MovieDetails from "./MovieDetails/MovieDetails";
+
 const Home = () => {
     return <>
         <Header />
