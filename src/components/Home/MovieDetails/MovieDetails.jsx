7f963c114b8583ccfd10e7c4f5d95ee4a4e924df
Karmel <karmeltajeddini82@gmail.com>
2024-02-12 03:51:56 +0330

    Add star rating to movieDetails

--- src/components/Home/MovieDetails/MovieDetails.jsx
+++ src/components/Home/MovieDetails/MovieDetails.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import './MovieDetails.css'
+import Box from "@mui/material/Box";
+import Rating from '@mui/material/Rating';
 
 const MovieDetails = ({ movie }) => {
     return <>
-        <div className="movie_card" id="bright">
+        <Box className="movie_card" id="bright">
             <div className="info_section">
                 <div className="movie_header">
                     <img className="locandina" src={`${movie.postImg}`} alt={movie.title} />
                     <h1>{movie.title}</h1>
                     <h4>{movie.year}, {movie.director}</h4>
                     <span className="minutes">{movie.duration}</span>
+                    <Box sx={{display: 'flex', flexDirection: 'row'}}>
+                        <Rating name="read-only" value={movie.rate} precision={0.1} readOnly sx={{ display: 'flex', py: 1.5 }} />
+                        <span className="rating"> {movie.rate} / 5 </span>
+                    </Box>
+
                 </div>
                 <div className="movie_desc">
                     <p className="text">
@@ -18,8 +25,8 @@ const MovieDetails = ({ movie }) => {
                 </div>
             </div>
 
-            <div className="blur_back" style={{backgroundImage: `url(${movie.backdropImg})`}}></div>
-        </div>
+            <div className="blur_back" style={{ backgroundImage: `url(${movie.backdropImg})` }}></div>
+        </Box>
     </>
 }
 
