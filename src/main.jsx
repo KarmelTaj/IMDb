ac5ce0048d7499f992e5199a0e059738551520cc
Karmel <karmeltajeddini82@gmail.com>
2024-02-12 10:55:08 +0330

    add router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import SignIn from './components/SignIn/SignIn.jsx'
+import SignUp from './components/SignUp/SignUp.jsx';
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
