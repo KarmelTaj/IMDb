17c21b0f7d48bdcb52558d90b59928bbbfc2e014
Karmel <karmeltajeddini82@gmail.com>
2024-02-15 16:53:31 +0330

    making movie details page

--- src/components/Movies/MovieDetailsPage.jsx
+++ src/components/Movies/MovieDetailsPage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { getID } from "../../utils/httpClient";
-
-const MovieDetailsPage = () =>{
+import { Box } from "@mui/material";
+const MovieDetailsPage = () => {
     const { movie } = useLoaderData();
-    
-     // Check if movie is an array and get the first element
+
+    // Check if movie is an array and get the first element
     const theMovie = Array.isArray(movie) ? movie[0] : movie;
-    
-    return <div>
-        {theMovie.title}
-    </div>
+
+    return <Box sx={{ height: '100vh ' }}>
+        <Box sx={{
+            height: '350px',
+            margin: '0',
+            position: 'relative',
+            overflow: 'hidden',
+            zIndex: 1
+        }}>
+
+        </Box>
+
+    </Box>
 }
 
 export default MovieDetailsPage
@@ -18,5 +27,5 @@ export default MovieDetailsPage
 
 export async function loader({ params }) {
     const movie = await getID(`/movies/${params.movieID}`);
-    return  { movie } ;
-  }
+    return { movie };
+}
