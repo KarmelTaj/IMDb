9aa1878103e033a10870127d4d15feb03d4543a6
Karmel <karmeltajeddini82@gmail.com>
2024-02-16 14:36:26 +0330

    add genres and stars to MovieDetailsOage

--- src/components/Movies/MovieDetailsPage.jsx
+++ src/components/Movies/MovieDetailsPage.jsx
@@ -7,10 +7,10 @@ import './MovieDetailsPage.css'
 
 const MovieDetailsPage = () => {
     const { movie } = useLoaderData();
-
     // Check if movie is an array and get the first element
-    const theMovie = Array.isArray(movie) ? movie[0] : movie;
-
+    const theMovie = Array.isArray(movie) ? movie[0] : movie
+    const [starname, setStarname] = useState(0)
+    console.log(theMovie)
     return <>
         <Box className="movie-card">
             <Box className="container">
@@ -20,8 +20,7 @@ const MovieDetailsPage = () => {
                         <Box className="details">
                             <Typography variant="h4" className="title">{theMovie.title}</Typography>
                             <span className="minutes">{theMovie.duration}</span>
-                            <span className="mpa">PG-13</span>
-
+                            <span className="mpa">{theMovie.mpa}</span>
                         </Box>
                     </Box>
                 </Box>
@@ -31,16 +30,19 @@ const MovieDetailsPage = () => {
                             <Rating name="read-only" value={+theMovie.rate} precision={0.1} size="large" readOnly sx={{ alignItems: 'center' }} />
                             <span className="rating"> {theMovie.rate} / 5 </span>
                         </Box>
-                        <span className="tag">action</span>
-                        <span className="tag">fantasy</span>
-                        <span className="tag">adventure</span>
+                        {theMovie.genres && theMovie.genres.map((genre, index) => (
+                            <span key={index} className="tag">{genre}</span>
+                        ))}
                     </Box>
                     <Box className="actors">
                         <Typography variant="body2" sx={{ fontSize: "17px " }}>{theMovie.description}</Typography>
                         <Box className="avatars">
-                            <Box className="tooltip"><img className="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar1.png" alt="avatar1" /><span className="tooltiptext">grewon.pdf</span></Box>
-                            <Box className="tooltip"><img className="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar2.png" alt="avatar2" /><span className="tooltiptext">grewon.pdf</span></Box>
-                            <Box className="tooltip"><img className="avatar" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/hobbit_avatar3.png" alt="avatar3" /><span className="tooltiptext">grewon.pdf</span></Box>
+                            {theMovie.stars && theMovie.stars.map((star, index) => (
+                                <Box key={index} className="tooltip">
+                                    <img className="avatar" src={star.picture} alt="Star Picture" />
+                                    <span className="tooltiptext">{star.name}</span>
+                                </Box>
+                            ))}
                         </Box>
                     </Box>
                 </Box>
