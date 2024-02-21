60d25f61572eb7272c1714cb83a65b257a1e2986
Karmel <karmeltajeddini82@gmail.com>
2024-02-21 18:19:38 +0330

    change link to useNavigate

--- src/components/Header/Login-button.jsx
+++ src/components/Header/Login-button.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import Link from '@mui/material/Link';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import { Zoom } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { useNavigate } from "react-router-dom";
+
+
+
 
 const LoginButton = ({ loggedIn }) => {
+    const navigate = useNavigate();
 
+    const navigateToHome = () => {
+        navigate(loggedIn ? "/" : '/login');
+    };
     const handleClick = () => {
         loggedIn ? localStorage.setItem("userAuth", null) : undefined;
     }
 
-    return <Link href={loggedIn ? "/" : "/login"} >
+    return <Box onClick={navigateToHome}>
         <Tooltip TransitionComponent={Zoom} title={loggedIn ? "Log Out" : "Go to your account"} >
             <IconButton aria-label="login" size="large" color="icon" onClick={handleClick} >
                 {loggedIn ?
@@ -21,7 +28,7 @@ const LoginButton = ({ loggedIn }) => {
                     <AccountCircleIcon sx={{ width: "2em", height: "2em" }} />}
             </IconButton>
         </Tooltip>
-    </Link>
+    </Box>
 }
 
 export default LoginButton
